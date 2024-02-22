28d072788753ed06fd12df2f28a0707fd82e6ac9
amankwaahjackson <amankwaahjackson@gmail.com>
2024-02-22 15:24:10 -0800

    simple_shell project

---
+++ Betty_checks/Name_complement.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**********
+This project deploys the Betty Style 
+We are doing this for our first project
+
+**********/
+
+int main()
+{
+	char name[31];
+
+	printf("Please put your name here: ");
+	scanf("%s", name);
+
+	printf("Hello %s\nYou are beautiful!", name);
+	return 0;
+}
